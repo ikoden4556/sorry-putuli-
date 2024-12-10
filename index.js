a14ef4081d2c7b6b4c1da4c5f00bf95b48fd08ac
ikoden4556 <ikoden4556@gmail.com>
2024-12-10 17:42:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-
-// Show buttons immediately after the page loads
 document.addEventListener("DOMContentLoaded", () => {
-    const btnGroup = document.querySelector(".btn-group");
-    btnGroup.style.display = "flex"; // Show buttons immediately
-});
+    const wrapper = document.querySelector(".wrapper");
+    const question = document.querySelector(".question");
+    const gif = document.querySelector(".gif");
+    const yesBtn = document.querySelector(".yes-btn");
+    const noBtn = document.querySelector(".no-btn");
 
-// Event listener for "Yes" button
-yesBtn.addEventListener("click", () => {
-    gif.src = "yes.gif"; // Change gif to "yes" when user clicks Yes
-    question.innerHTML = "Yay! Thank youuuuuuuuu >.<"; // Update question
-});
+    // Event listener for "Yes" button
+    yesBtn.addEventListener("click", () => {
+        gif.src = "yes.gif"; // Change gif to "yes" when user clicks Yes
+        question.innerHTML = "Yay! Thank youuuuuuuuu >.<"; // Update question
+    });
 
-// Event listener for "No" button
-noBtn.addEventListener("click", () => {
-    gif.src = "no.gif"; // Change gif to "no" when user clicks No
-    question.innerHTML = "Oh no... Maybe next time."; // Update question
+    // Event listener for "No" button
+    noBtn.addEventListener("click", () => {
+        gif.src = "no.gif"; // Change gif to "no" when user clicks No
+        question.innerHTML = "Oh no... Maybe next time."; // Update question
+    });
 });
